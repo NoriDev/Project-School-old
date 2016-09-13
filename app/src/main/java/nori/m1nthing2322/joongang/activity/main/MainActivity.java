248bfe0660ca0acca662dea632094c8b6117a6bc
NoriDev <m1nthing2322@gmail.com>
2016-09-13 20:53:32 +0900

    Initial version

---
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/main/MainActivity.java
@@ -0,0 +1,243 @@
+package nori.m1nthing2322.joongang.activity.main;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.StrictMode;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import nori.m1nthing2322.joongang.R;
+import nori.m1nthing2322.joongang.activity.settings.SettingsActivity;
+import nori.m1nthing2322.joongang.tool.Preference;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int ver= 10110;
+
+    private ProgressDialog dialog;
+    String xml;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.mToolbar);
+        setSupportActionBar(mToolbar);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.mViewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.mTabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+
+        showUpdateNotification();
+    }
+
+    private void showUpdateNotification() {
+        try {
+            Preference mPref = new Preference(getApplicationContext());
+            PackageManager packageManager = getPackageManager();
+            PackageInfo info = packageManager.getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
+
+            int versionCode = info.versionCode;
+
+            if (mPref.getInt("versionCode", 0) != versionCode) {
+                mPref.putInt("versionCode", versionCode);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
+                builder.setTitle(R.string.update_notification_title);
+                builder.setMessage(R.string.update_notification_message);
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.show();
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setCancelable(false);
+        dialog.setMessage(getString((R.string.check_version)));
+        dialog.show();
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        StringBuilder sBuffer = new StringBuilder();
+        try{//Start Try
+            String urlAddr = "http://devhost.iptime.org:1479/html/joonganghs.xml";
+            URL url = new URL(urlAddr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            if(conn != null){//Start if
+                conn.setConnectTimeout(20000);
+                //conn.setUseCaches(false);
+                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK){//Start if
+                    InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+                    while(true){//Start While
+                        String line = br.readLine();
+                        if(line==null){//Start if
+                            break;
+                        }//end if
+                        sBuffer.append(line);
+                    }//end while
+                    br.close();
+                    conn.disconnect();
+                }//end if
+            }//end if
+            xml = sBuffer.toString();
+            CountDownTimer _timer = new CountDownTimer(1000, 1000){
+                public void onTick(long millisUntilFinished)
+                {}
+                public void onFinish(){
+                    dialog.dismiss();
+                    if(Integer.parseInt(xml)==ver){//new version
+                        Toast.makeText(getApplicationContext(), R.string.latest_version, Toast.LENGTH_SHORT).show();
+
+                    }
+                    else if(Integer.parseInt(xml)>ver){//현재버전보다 서버버전이 높을때
+                        AlertDialog.Builder builder = new
+                                AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle(R.string.low_version);
+                        builder.setMessage(R.string.plz_update);
+                        builder.setCancelable(false);
+                        builder.setNegativeButton(R.string.later, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }});
+                        builder.setPositiveButton(R.string.update_now, new
+                                DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        Intent myIntent = new Intent
+                                                (Intent.ACTION_VIEW, Uri.parse
+                                                        ("https://play.google.com/store/apps/details?id=nori.m1nthing2322.joongang"));
+                                        startActivity(myIntent);
+                                    }});
+                        builder.show();
+                    }
+                    else {//현재버전보다 서버 버전이 낮을때
+
+                        AlertDialog.Builder builder = new
+                                AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle(R.string.crack);
+                        builder.setMessage(R.string.crack_contents);
+                        builder.setCancelable(false);
+                        builder.setPositiveButton(R.string.access, new
+                                DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }});
+                        builder.show();
+                    }
+                }
+            };
+            _timer.start();
+        }//end try
+        catch (Exception e) {//네트워크가 올바르지 않을때
+            Toast.makeText(getApplicationContext(), R.string.offline, Toast.LENGTH_LONG).show();
+
+            dialog.cancel();
+        }
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter mAdapter = new Adapter(getSupportFragmentManager());
+
+        mAdapter.addFragment(getString(R.string.activity_main_fragment_simple), MainFragment.getInstance(1));
+        mAdapter.addFragment(getString(R.string.activity_main_fragment_detailed), MainFragment.getInstance(2));
+
+        viewPager.setAdapter(mAdapter);
+    }
+
+    class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        void addFragment(String mTitle, Fragment mFragment) {
+            mFragments.add(mFragment);
+            mFragmentTitles.add(mTitle);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_feedback) {
+            Uri uri = Uri.parse("mailto:noridevdroid@gmail.com");
+            Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+            startActivity(it);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/main/MainFragment.java
@@ -0,0 +1,148 @@
+package nori.m1nthing2322.joongang.activity.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import nori.m1nthing2322.joongang.R;
+import nori.m1nthing2322.joongang.activity.bap.BapActivity;
+import nori.m1nthing2322.joongang.activity.changelog.ChangelogActivity;
+import nori.m1nthing2322.joongang.activity.notice.NoticeActivity;
+import nori.m1nthing2322.joongang.activity.timetable.TimeTableActivity;
+import nori.m1nthing2322.joongang.activity.changelog.ChangelogBetaActivity;
+import nori.m1nthing2322.joongang.tool.BapTool;
+import nori.m1nthing2322.joongang.tool.Preference;
+import nori.m1nthing2322.joongang.tool.RecyclerItemClickListener;
+import nori.m1nthing2322.joongang.tool.TimeTableTool;
+
+/**
+ * Created by whdghks913 on 2015-11-30.
+ */
+public class MainFragment extends Fragment {
+
+    public static Fragment getInstance(int code) {
+        MainFragment mFragment = new MainFragment();
+
+        Bundle args = new Bundle();
+        args.putInt("code", code);
+        mFragment.setArguments(args);
+
+        return mFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recyclerview, container, false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+
+        final MainAdapter mAdapter = new MainAdapter(getActivity());
+        recyclerView.setAdapter(mAdapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View mView, int position) {
+                boolean isSimple = mAdapter.getItemData(position).isSimple;
+
+                if (isSimple) {
+                    switch (position) {
+                        case 0:
+                            startActivity(new Intent(getActivity(), BapActivity.class));
+                            break;
+                        case 1:
+                            startActivity(new Intent(getActivity(), TimeTableActivity.class));
+                            break;
+                    }
+                } else {
+                    switch (position) {
+						case 0:
+                            Toast.makeText(getActivity(), "학교 공지 기능 개발을 위해 준비중입니다:)", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getActivity(), ScheduleActivity.class));
+                            break;
+                        case 1:
+                            startActivity(new Intent(getActivity(), NoticeActivity.class));
+                            break;
+						case 2:
+                            startActivity(new Intent(getActivity(), ChangelogActivity.class));
+                            break;
+/*						case 2:
+                            startActivity(new Intent(getActivity(), ChangelogBetaActivity.class));
+                            break;
+*/
+						case 3:
+                            Toast.makeText(getActivity(), "2016년 일정 기능은 아직 준비중입니디:)", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getActivity(), ScheduleActivity.class));
+                            break;
+                        case 4:
+							Toast.makeText(getActivity(), "시험기간이 아닙니다.\n시험기간에만 사용가능합니다.", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getActivity(), ExamTimeActivity.class));
+                            break;
+                        case 5:
+                            break;
+                    }
+                }
+            }
+        }));
+
+        Bundle args = getArguments();
+        int code = args.getInt("code");
+        Preference mPref = new Preference(getActivity());
+
+        if (code == 1) {
+            // SimpleView
+            if (mPref.getBoolean("simpleShowBap", true)) {
+                BapTool.todayBapData mBapData = BapTool.getTodayBap(getActivity());
+                mAdapter.addItem(R.drawable.rice,
+                        getString(R.string.title_activity_bap),
+                        getString(R.string.message_activity_bap),
+                        mBapData.title,
+                        mBapData.info);
+            } else {
+                mAdapter.addItem(R.drawable.rice,
+                        getString(R.string.title_activity_bap),
+                        getString(R.string.message_activity_bap), true);
+            }
+
+            if (mPref.getBoolean("simpleShowTimeTable", true)) {
+                TimeTableTool.todayTimeTableData mTimeTableData = TimeTableTool.getTodayTimeTable(getActivity());
+                mAdapter.addItem(R.drawable.timetable,
+                        getString(R.string.title_activity_time_table),
+                        getString(R.string.message_activity_time_table),
+                        mTimeTableData.title,
+                        mTimeTableData.info);
+            } else {
+                mAdapter.addItem(R.drawable.timetable,
+                        getString(R.string.title_activity_time_table),
+                        getString(R.string.message_activity_time_table), true);
+            }
+        } else {
+            // DetailedView
+			mAdapter.addItem(R.drawable.notice,
+					getString(R.string.title_activity_scnoti),
+					getString(R.string.message_activity_scnoti));
+			mAdapter.addItem(R.drawable.notice,
+					getString(R.string.title_activity_notice),
+					getString(R.string.message_activity_notice));
+           /* mAdapter.addItem(R.drawable.notice,
+                    getString(R.string.title_activity_changelog),
+                    getString(R.string.message_activity_changelog));*/
+			mAdapter.addItem(R.drawable.notice,
+                    getString(R.string.title_activity_changelog),
+//					getString(R.string.title_activity_changelog_beta),
+					getString(R.string.message_activity_changelog));
+            mAdapter.addItem(R.drawable.calendar,
+                    getString(R.string.title_activity_schedule),
+                    getString(R.string.message_activity_schedule));
+            mAdapter.addItem(R.drawable.ic_launcher_big,
+                    getString(R.string.title_activity_exam_range),
+                    getString(R.string.message_activity_exam_range));
+        }
+
+        return recyclerView;
+    }
+}
