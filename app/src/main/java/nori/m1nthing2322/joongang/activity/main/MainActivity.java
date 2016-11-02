c983bb072c8c2264ca9971872148a34d4c6e81cb
NoriDev <m1nthing2322@gmail.com>
2016-11-02 20:55:41 +0900

    2.1a version up

--- app/src/main/java/nori/m1nthing2322/joongang/activity/main/MainActivity.java
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/main/MainActivity.java
@@ -37,7 +37,7 @@ import nori.m1nthing2322.joongang.tool.Preference;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private int ver= 21102;
+//    private int ver= 21110;
 
 //    private BackPressCloseHandler backPressCloseHandler;
 
