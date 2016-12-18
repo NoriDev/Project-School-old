ff86b94b15e03a931bc385fd8c92c0810227cd35
NoriDev <m1nthing2322@gmail.com>
2016-12-18 11:07:52 +0900

    end_Exam Text edit

--- app/src/main/java/nori/m1nthing2322/joongang/activity/exam/ExamTimeActivity.java
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/exam/ExamTimeActivity.java
@@ -42,9 +42,8 @@ public class ExamTimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_time);
 
-        AlertDialog.Builder end_Exam = new AlertDialog.Builder(this, R.style.AppCompatErrorAlertDialogStyle);
-        end_Exam.setTitle("시험이 끝났습니다 (짝짝짝)");
-        end_Exam.setMessage("드디어 마지막 시험이 끝났습니다!\n시험 치시느라 고생하셨고, 이제 얼마 남지 않은 담임선생님과의 시간을 소중히 간직하는 시간을 가져봅시다 :)");
+        AlertDialog.Builder end_Exam = new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
+        end_Exam.setMessage("드디어 마지막 시험이 끝났습니다! (짝짝짝)\n시험 치시느라 고생많으셨고, 이제 얼마 남지 않은 담임선생님과의 시간을 소중히 간직하는 시간을 가져봅시다 :)");
         end_Exam.setPositiveButton(android.R.string.ok, null);
         end_Exam.show();
 
