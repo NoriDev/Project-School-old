03e0766861c60be2f7311809c18d657a7f596c03
Nori <m1nthing2322@gmail.com>
2017-03-04 22:28:57 +0900

    Delete spacing

--- app/src/main/java/nori/m1nthing2322/joongang/activity/bap/BapActivity.java
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/bap/BapActivity.java
@@ -152,7 +152,6 @@ public class BapActivity extends AppCompatActivity {
                     builder.setCancelable(false);
                     builder.show();
                 }
-
                 return;
             }
 
