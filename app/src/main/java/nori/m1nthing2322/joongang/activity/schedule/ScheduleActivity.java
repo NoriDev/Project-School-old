f0d2c7e5f48e7d181190d2c1d6b92a0463e208f5
NoriDev <m1nthing2322@gmail.com>
2017-04-28 23:54:18 +0900

    서버에서 업데이트 유무를 확인하도록 변경

--- app/src/main/java/nori/m1nthing2322/joongang/activity/schedule/ScheduleActivity.java
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/schedule/ScheduleActivity.java
@@ -1,31 +1,41 @@
 package nori.m1nthing2322.joongang.activity.schedule;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.StrictMode;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import nori.m1nthing2322.joongang.R;
-import nori.m1nthing2322.joongang.tool.Preference;
 
 /**
  * Created by whdghks913 on 2015-12-10.
  */
 public class ScheduleActivity extends AppCompatActivity {
     ViewPager viewPager;
+
+    private int schedulever= 201701; // yyyy년도 nn버전 (01 버전 - 일정표에 수정이 가해지지 않음, 02~ 버전 - 일정표가 일부 또는 전체 수정이 가해짐)
+    String xml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,29 +69,70 @@ public class ScheduleActivity extends AppCompatActivity {
 
         setCurrentItem();
 
-        showScheduleNotification();
+//        showScheduleNotification();
     }
 
-    private void showScheduleNotification() {
-        try {
-            Preference mPref = new Preference(getApplicationContext());
-            PackageManager packageManager = getPackageManager();
-            PackageInfo info = packageManager.getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
-
-            int scheduleCode = 1;
-
-            /*
-            if (mPref.getInt("scheduleCode", 0) != scheduleCode) {
-                mPref.putInt("scheduleCode", scheduleCode);
-                AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
-                builder.setTitle("일정 관련 안내");
-                builder.setMessage("9월 이후 일정은 학교에서 발표하는대로 추가할 예정이며, 9월 이후 일정은 [대한민국 공휴일]만 표시됩니다.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.show();
-            }
-            */
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+    private void examTimeTableUpdate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        StringBuilder sBuffer = new StringBuilder();
+        try{//Start Try
+            String urlAddr = "http://noridev.iptime.org/Project%20School/Jinhae%20Joongang%20High%20School/Project_School_JoongangHS_Schedule.xml";
+            URL url = new URL(urlAddr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            if(conn != null){//Start if
+                conn.setConnectTimeout(20000);
+                //conn.setUseCaches(false);
+                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK){//Start if
+                    InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+                    while(true){//Start While
+                        String line = br.readLine();
+                        if(line==null){//Start if
+                            break;
+                        }//end if
+                        sBuffer.append(line);
+                    }//end while
+                    br.close();
+                    conn.disconnect();
+                }//end if
+            }//end if
+            xml = sBuffer.toString();
+            CountDownTimer _timer = new CountDownTimer(1000, 1000){
+                public void onTick(long millisUntilFinished)
+                {}
+                public void onFinish(){
+                    if(Integer.parseInt(xml)==schedulever){//new version
+//                        Toast.makeText(getApplicationContext(), R.string.latest_version, Toast.LENGTH_SHORT).show();
+                    } else if(Integer.parseInt(xml)>schedulever){
+                        //현재 버전보다 서버 버전이 높을때
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ScheduleActivity.this);
+                        builder.setTitle("학사일정이 업데이트됨");
+                        builder.setMessage("학사일정이 업데이트 됨에 따라, 기존 일정표를 업데이트 하셔야 합니다.\n일정표를 업데이트 하시려면 \'확인\'을 눌러주십시오.\n\n일정을 업데이트 하시려면, 먼저 앱을 업데이트 하셔야 합니다.");
+                        builder.setCancelable(false);
+                        builder.setNegativeButton(R.string.later, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }});
+                        builder.setPositiveButton(R.string.update_now, new
+                                DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent myIntent = new Intent
+                                                (Intent.ACTION_VIEW, Uri.parse
+                                                        ("https://play.google.com/store/apps/details?id=nori.m1nthing2322.joongang"));
+                                        startActivity(myIntent);
+                                    }});
+                        builder.setCancelable(false);
+                        builder.show();
+                    }else {
+                        //현재 버전보다 서버 버전이 낮을때
+                    }
+                }
+            };
+            _timer.start();
+        }//end try
+        catch (Exception e) {
+            //네트워크가 올바르지 않을때
         }
     }
 
