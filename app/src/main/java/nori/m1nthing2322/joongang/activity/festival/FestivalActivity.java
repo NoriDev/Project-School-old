dbfd19d0523d3b6b30964d293f5ae32b4c0bc61b
NoriDev <m1nthing2322@gmail.com>
2017-04-29 14:08:52 +0900

    행사 확인 기능 추가

---
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/festival/FestivalActivity.java
@@ -0,0 +1,174 @@
+package nori.m1nthing2322.joongang.activity.festival;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.StrictMode;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import nori.m1nthing2322.joongang.R;
+
+/**
+ * Created by whdghks913 on 2015-12-10.
+ */
+public class FestivalActivity extends AppCompatActivity {
+    ViewPager viewPager;
+
+    private int festivalVer= 201701; // yyyy년도 nn버전 (01 버전 - 일정표에 수정이 가해지지 않음, 02~ 버전 - 일정표가 일부 또는 전체 수정이 가해짐)
+    String xml;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_festival);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.mToolbar);
+        setSupportActionBar(mToolbar);
+
+        ActionBar mActionBar = getSupportActionBar();
+        if (mActionBar != null) {
+            mActionBar.setHomeButtonEnabled(true);
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setSubtitle("2017년 행사일정");
+
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+        }
+
+        viewPager = (ViewPager) findViewById(R.id.mViewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.mTabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+
+        festivalUpdate();
+    }
+
+    private void festivalUpdate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        StringBuilder sBuffer = new StringBuilder();
+        try{//Start Try
+            String urlAddr = "http://noridev.iptime.org/Project%20School/Jinhae%20Joongang%20High%20School/Project_School_JoongangHS_Schedule.xml";
+            URL url = new URL(urlAddr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            if(conn != null){//Start if
+                conn.setConnectTimeout(20000);
+                //conn.setUseCaches(false);
+                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK){//Start if
+                    InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+                    while(true){//Start While
+                        String line = br.readLine();
+                        if(line==null){//Start if
+                            break;
+                        }//end if
+                        sBuffer.append(line);
+                    }//end while
+                    br.close();
+                    conn.disconnect();
+                }//end if
+            }//end if
+            xml = sBuffer.toString();
+            CountDownTimer _timer = new CountDownTimer(1000, 1000){
+                public void onTick(long millisUntilFinished)
+                {}
+                public void onFinish(){
+                    if(Integer.parseInt(xml)==festivalVer){//new version
+//                        Toast.makeText(getApplicationContext(), R.string.latest_version, Toast.LENGTH_SHORT).show();
+                    } else if(Integer.parseInt(xml)>festivalVer){
+                        //현재 버전보다 서버 버전이 높을때
+                        AlertDialog.Builder builder = new AlertDialog.Builder(FestivalActivity.this);
+                        builder.setTitle("학사일정이 업데이트됨");
+                        builder.setMessage("학사일정이 업데이트 됨에 따라, 기존 일정표를 업데이트 하셔야 합니다.\n일정표를 업데이트 하시려면 \'확인\'을 눌러주십시오.\n\n일정을 업데이트 하시려면, 먼저 앱을 업데이트 하셔야 합니다.");
+                        builder.setCancelable(false);
+                        builder.setNegativeButton(R.string.later, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }});
+                        builder.setPositiveButton(R.string.update_now, new
+                                DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent myIntent = new Intent
+                                                (Intent.ACTION_VIEW, Uri.parse
+                                                        ("https://play.google.com/store/apps/details?id=nori.m1nthing2322.joongang"));
+                                        startActivity(myIntent);
+                                    }});
+                        builder.show();
+                    } else {
+                        //현재 버전보다 서버 버전이 낮을때
+                    }
+                }
+            };
+            _timer.start();
+        }//end try
+        catch (Exception e) {
+            //네트워크가 올바르지 않을때
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter mAdapter = new Adapter(getSupportFragmentManager());
+
+        mAdapter.addFragment("교내대회", FestivalFragment.getInstance(1));
+        mAdapter.addFragment("체육대회", FestivalFragment.getInstance(2));
+
+        viewPager.setAdapter(mAdapter);
+    }
+
+    class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        public void addFragment(String mTitle, Fragment mFragment) {
+            mFragments.add(mFragment);
+            mFragmentTitles.add(mTitle);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
+}
