d814444ab98bb7838b86a7a1de10dc44997e6964
NoriDev <m1nthing2322@gmail.com>
2017-06-11 21:33:18 +0900

    4.0 정식 버전 대응

--- app/src/main/java/nori/m1nthing2322/joongang/activity/changelog/ChangelogActivity.java
+++ app/src/main/java/nori/m1nthing2322/joongang/activity/changelog/ChangelogActivity.java
@@ -24,6 +24,14 @@ import nori.m1nthing2322.joongang.tool.Preference;
 import nori.m1nthing2322.joongang.tool.TimeTableTool;
 import nori.m1nthing2322.joongang.tool.Tools;
 
+/**
+ * 이 소스는 원작자: Mir(whdghks913)에 의해 생성되었으며,
+ * 2차 수정자: NoriDev(noridevdroid@gmail.com)에 의해 수정되었습니다.
+ *
+ * 본 소스를 사용하고자 한다면, 이 주석을 삭제 또는 수정해서는 안됩니다.
+ * 또한 앱 내부 및 스토어 등록 정보에서 다른 사람이 볼 수 있는 곳에 적어도 하나 이상의 위치에 위 저작자가 표시되어야 합니다.
+ */
+
 public class ChangelogActivity extends AppCompatActivity {
     public static final String ChangelogDBName = "Changelog.db";
     public static final String ChangelogTableName = "ChangelogInfo";
@@ -94,7 +102,7 @@ public class ChangelogActivity extends AppCompatActivity {
 
         if (Tools.isOnline(getApplicationContext())) {
             getNoticeDownloadTask mTask = new getNoticeDownloadTask();
-            mTask.execute("https://docs.google.com/spreadsheets/d/1hTI5-CFLTV_2yUO6shkFv1quoHJFTlBx7WxjS9KOwGg/pubhtml?gid=1473912750&single=true");
+            mTask.execute("https://docs.google.com/spreadsheets/d/1hTI5-CFLTV_2yUO6shkFv1quoHJFTlBx7WxjS9KOwGg/pubhtml?gid=1291394643&single=true");
         } else {
             offlineData();
             Toast.makeText(getApplicationContext(), "네트워크에 연결되어 있지않아,\n오프라인 모드로만 볼 수 있습니다.", Toast.LENGTH_SHORT).show();
